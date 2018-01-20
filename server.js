bd95a0c65d71669c74688b495fb00f19f849d884
Ariel Tapia <arieltapia16@gmail.com>
2018-01-20 14:07:38 -0300

    set up to heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express =  require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -46,7 +48,7 @@ app.get('/about' , (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('server is running')
+app.listen(port, () => {
+  console.log('server is running on port ' + port)
 });
  
